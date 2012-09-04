1f1aa278c505a1df64c3a4671d447c1bce6f3aa8
unknown <hp@Vicky.(none)>
2012-09-04 19:06:32 +0530

    init

--- constants.php
+++ constants.php
@@ -1,5 +1,5 @@
 <?php
-  define("DB_SERVER","instance25817.db.xeround.com:16926");
+  define("DB_SERVER","int.instance25817.db.xeround.com:16926");
   define("DB_USER","vikas");
   define("DB_PASS","vikas");
   define("DB_NAME","gravitas");
